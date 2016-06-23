cd7f0d982b51ecdb8afd2386f49bda72f33981a9
Trevor Whitney <trevorjwhitney@gmail.com>
2016-06-22 18:43:36 -0600

    Add bash_it and .bash_profile

--- install.sh
+++ install.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+if [ ! -e ~/.bash_it ]; then
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+
+  case $OSTYPE in
+    darwin*)
+      CONFIG_FILE=.bash_profile
+      ;;
+    *)
+      CONFIG_FILE=.bashrc
+      ;;
+  esac
+
+  if [ -e "$HOME/$CONFIG_FILE" ]; then
+    rm -f "$HOME/$CONFIG_FILE"
+  fi
+
+  if [ -e "$HOME/$CONFIG_FILE.bak" ]; then
+    rm -f "$HOME/$CONFIG_FILE.bak"
+  fi
+
+  ~/.bash_it/install.sh
+fi
+
 create_link() {
     if [ -e ~/$1 ]; then
         rm -rf ~/$1;
@@ -14,6 +37,7 @@ create_link .gitconfig
 create_link .tmux.conf
 create_link .vimrc
 create_link .ideavimrc
+create_link .bash_profile
 
 create_vim_link() {
     if [ -e ~/.vim/$1 ]; then
