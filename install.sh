f223ad1e2b20af8f3f774d42fecfbf20ba9fe4a7
Trevor Whitney <twhitney@pivotal.io>
2016-11-07 15:40:44 -0700

    add kotlin config

--- install.sh
+++ install.sh
@@ -61,6 +61,7 @@ create_vim_link vimrc.ruby.bundles
 create_vim_link vimrc.ruby.config
 create_vim_link vimrc.haskell.bundles
 create_vim_link vimrc.haskell.config
+create_vim_link vimrc.kotlin.bundles
 
 if [ ! -e "$HOME/.vim/bundle" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
