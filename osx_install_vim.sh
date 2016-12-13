d7416987141aad8b47ff0794241b07fdd70faba3
Trevor Whitney <trevorjwhitney@gmail.com>
2016-12-13 15:03:27 -0700

    added git completion and direnv

--- osx_install_vim.sh
+++ osx_install_vim.sh
@@ -3,8 +3,7 @@
 mkdir ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 brew update
-brew install vim
-brew install reattach-to-user-namespace
+brew install vim reattach-to-user-namespace direnv
 
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
