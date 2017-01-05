0dae2da619889d11fc0433d82c0eee23a2c976d4
Trevor Whitney <twhitney@pivotal.io>
2017-01-05 13:51:15 -0700

    show hidden files by default

--- osx_install_vim.sh
+++ osx_install_vim.sh
@@ -12,3 +12,4 @@ brew install reattach-to-user-namespace direnv
 
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write com.apple.finder AppleShowAllFiles YES
