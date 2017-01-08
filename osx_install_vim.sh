397f9c14b4b3ca518f088e58ba09c1481220f61b
Trevor Whitney <twhitney@pivotal.io>
2017-01-08 09:53:18 -0700

    install envsubst on osx for templating

--- osx_install_vim.sh
+++ osx_install_vim.sh
@@ -8,7 +8,8 @@ fi
 
 brew update
 brew install vim --with-override-system-vi
-brew install reattach-to-user-namespace direnv
+brew install reattach-to-user-namespace direnv gettext
+brew link --force gettext
 
 defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
