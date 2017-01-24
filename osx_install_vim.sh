be2718a57129eb5023b3fdf717603d3f56a3e40f
Trevor Whitney <twhitney@pivotal.io>
2017-01-24 16:50:14 -0700

    install vimline, tmuxline, bufferline, and powerline fonts

--- osx_install_vim.sh
+++ osx_install_vim.sh
@@ -6,6 +6,15 @@ if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+# Intalls powerline fonts
+mkdir -p ~/tmp
+pushd ~/tmp
+  git clone https://github.com/powerline/fonts.git
+  cd fonts
+  ./install.sh
+popd
+rm -rf ~/tmp/fonts
+
 brew update
 brew install vim --with-override-system-vi
 brew install reattach-to-user-namespace direnv gettext the_silver_searcher
