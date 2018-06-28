0b268f5f7f7090174ae191d3bebaf9d98ea31ea5
Trevor Whitney <twhitney@HeyPumpkin.local>
2018-06-28 16:54:37 -0600

    update git-authors

--- git-authors/install.sh
+++ git-authors/install.sh
@@ -2,4 +2,12 @@
 
 dir=$(cd $(dirname $0); pwd)
 
-cp $dir/git-authors $HOME/.git-authors
+create_link() {
+    if [ -h "$HOME/.$1" ] || [ -e "$HOME/.$1" ]; then
+        rm -rf "$HOME/.$1";
+    fi
+
+    ln -s "$dir/$1" "$HOME/.$1"
+}
+
+create_link git-authors
