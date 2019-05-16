42fc9108693edbbdb172b2b9fc823f898e7c6a14
Trevor Whitney <trevorjwhitney@gmail.com>
2019-05-16 15:30:30 -0600

    add exit code to bash prompt

---
+++ bash-it/alias-git.bash
@@ -0,0 +1,14 @@
+export GIT_DUET_GLOBAL=true
+export GIT_DUET_ROTATE_AUTHOR=1
+
+alias duet='git duet --global'
+alias solo='git solo --global'
+
+alias g='git'
+alias gpr='git pull -r'
+alias gpp='git pull -r && git push'
+alias gs='git status'
+alias gst='git status'
+
+alias gap="git add -p"
+alias gdf="git diff --color | diff-so-fancy"
