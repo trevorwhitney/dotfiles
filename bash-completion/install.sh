c70f3f389606409501a465659d8a535dccda8727
Trevor Whitney <trevorjwhitney@gmail.com>
2019-06-06 09:05:26 -0600

    add gradle bash completion

---
+++ bash-completion/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+current_dir=$(cd $(dirname $0) && pwd)
+
+BASH_COMPLETION_DIR="$HOME/.bash_completion.d"
+
+# Linking files is good for configurations
+# we want to track changes to
+create_link() {
+    if [[ -h "${BASH_COMPLETION_DIR}/$1" ]] || [[ -e "${BASH_COMPLETION_DIR}/$1" ]]; then
+        rm -rf "${BASH_COMPLETION_DIR}/$1";
+    fi
+
+    ln -s "$current_dir/$1" "${BASH_COMPLETION_DIR}/$1"
+}
+
+
+mkdir -p ${BASH_COMPLETION_DIR}
+create_link gradle
