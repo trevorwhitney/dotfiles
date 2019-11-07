cda5d9081548391fcefb922f4b129670de9c8af6
Trevor Whitney <trevorjwhitney@gmail.com>
2019-11-07 15:39:43 -0700

    fix tmux and bash aliases for linux vs darwin

---
+++ bash-aliases-darwin/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+current_dir=$(cd $(dirname "$0") && pwd)
+dot_files_dir=$(cd "$current_dir/.." && pwd)
+
+source "$dot_files_dir/lib.sh"
+
+create_link "$current_dir/bash_aliases"
