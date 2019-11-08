17787f2f1c60a5669aadd3367c180342af43180e
Trevor Whitney <trevorjwhitney@gmail.com>
2019-11-08 14:23:04 -0700

    fix tmux configs

--- lib.sh
+++ lib.sh
@@ -4,10 +4,7 @@ current_dir=$(cd $(dirname $0) && pwd)
 # Linking files is good for configurations
 # we want to track changes to
 create_link() {
-    set -x
-    echo "Args: $1"
     file_name="$(basename "$1")"
-    echo "File name $file_name"
     if [ -h "$HOME/.$file_name" ] || [ -e "$HOME/.$file_name" ]; then
       rm -rf "$HOME/.$file_name";
     fi
@@ -19,9 +16,7 @@ create_link() {
 # have local overrides, since we don't want to track
 # the local overrides in version control
 copy_file() {
-    echo "Args: $1"
     file_name="$(basename "$1")"
-    echo "File name: $file_name"
     if [ ! -e "$HOME/.$file_name" ]; then
       cp "$1" "$HOME/.$file_name"
     fi
