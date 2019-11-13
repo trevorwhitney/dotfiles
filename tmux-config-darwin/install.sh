a5432e8e4e7a946837151319425d49552a9e7dee
Trevor Whitney <trevorjwhitney@gmail.com>
2019-11-13 10:01:24 -0700

    add community tmux config

--- tmux-config-darwin/install.sh
+++ tmux-config-darwin/install.sh
@@ -2,7 +2,9 @@
 
 current_dir=$(cd "$(dirname "$0")" && pwd)
 dot_files_dir=$(cd "$current_dir/.." && pwd)
+tmux_dir=$(cd "$dot_files_dir/.tmux" && pwd)
 
 source "$dot_files_dir/lib.sh"
 
-create_link "$current_dir/tmux.conf"
+create_link "$tmux_dir/.tmux.conf"
+create_link "$current_dir/tmux.conf.local"
