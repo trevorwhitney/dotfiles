c6d4c60110e1ae90e085b271b8fe7e3426ff5cf2
Trevor Whitney <trevorjwhitney@gmail.com>
2019-11-13 10:01:24 -0700

    add community tmux config

--- lib.sh
+++ lib.sh
@@ -4,7 +4,7 @@ current_dir=$(cd $(dirname $0) && pwd)
 # Linking files is good for configurations
 # we want to track changes to
 create_link() {
-    file_name="$(basename "$1")"
+    file_name="$(basename "$1" | sed "s/^\.//g")"
     if [ -h "$HOME/.$file_name" ] || [ -e "$HOME/.$file_name" ]; then
       rm -rf "$HOME/.$file_name";
     fi
@@ -16,7 +16,7 @@ create_link() {
 # have local overrides, since we don't want to track
 # the local overrides in version control
 copy_file() {
-    file_name="$(basename "$1")"
+    file_name="$(basename "$1" | sed "s/^\.//g")"
     if [ ! -e "$HOME/.$file_name" ]; then
       cp "$1" "$HOME/.$file_name"
     fi
