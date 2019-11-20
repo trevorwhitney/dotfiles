81c1f75d885f5d578841179b5d56766f5a8e63ea
Trevor Whitney <trevorjwhitney@gmail.com>
2019-11-19 20:07:43 -0700

    add evil mode and moe-theme to emacs, improve zsh vi mode

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -17,11 +17,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes
    (quote
-    ("9271c0ad73ef29af016032376d36e8aed4e89eff17908c0b578c33e54dfa1da1" default)))
- '(package-selected-packages (quote (helm solarized-light use-package solarized-theme))))
+    ("13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" default)))
+ '(package-selected-packages
+   (quote
+    (moe-theme 0blayout plan9-theme helm solarized-light use-package solarized-theme))))
 
 (tool-bar-mode -1)
 (server-start)
@@ -34,6 +35,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; enable evil mode
+(require 'key-chord)
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+(key-chord-mode 1)
+
+;; community managed defaults
 (add-to-list 'load-path "~/workspace/dotfiles/emacs/better-defaults")
 (require 'better-defaults)
 
+(load-theme 'moe-light t)
