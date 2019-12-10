6207c052bbd80346c0f469f8a284021eb3f11c88
Trevor Whitney <trevorjwhitney@gmail.com>
2019-12-10 09:09:55 -0700

    add aws-creds function to zsh

--- oh-my-zsh/rachio-functions.zsh
+++ oh-my-zsh/rachio-functions.zsh
@@ -77,7 +77,48 @@ function log-level() {
   return 0
 }
 
-function aws-login() {
-  local code="${1}"
-  aws sts get-session-token --serial-number arn:aws:iam::170902677803:mfa/twhitney --token-code ${code}
+function aws-creds () {
+    set -eu
+    local pkg=aws-creds
+    if [[ ! $1 ]]; then
+        echo "$pkg: missing required argument: MFA_TOKEN" 1>&2
+        return 99
+    fi
+
+    export AWS_SESSION_TOKEN
+    local iam_user=${AWS_IAM_USER}
+    local aws_account=${AWS_ACCOUNT}
+    local rv creds_json
+    creds_json=$(aws --output json sts get-session-token --duration-seconds 86400 --serial-number "arn:aws:iam::$aws_account:mfa/$iam_user" --token-code "$1")
+    rv="$?"
+    if [[ $rv -ne 0 || ! $creds_json ]]; then
+        echo "$pkg: failed to get credentials for user '$iam_user' account '$aws_account': $creds_json" 1>&2
+        return "$rv"
+    fi
+
+    local jq="jq --exit-status --raw-output"
+    AWS_ACCESS_KEY_ID=$(echo "$creds_json" | jq --exit-status --raw-output .Credentials.AccessKeyId)
+    rv="$?"
+    if [[ $rv -ne 0 || ! $AWS_ACCESS_KEY_ID ]]; then
+        echo "$pkg: failed to parse output for AWS_ACCESS_KEY_ID: $creds_json" 1>&2
+        return "$rv"
+    fi
+    AWS_SECRET_ACCESS_KEY=$(echo "$creds_json" | jq --exit-status --raw-output .Credentials.SecretAccessKey)
+    rv="$?"
+    if [[ $rv -ne 0 || ! $AWS_SECRET_ACCESS_KEY ]]; then
+        echo "$pkg: failed to parse output for AWS_SECRET_ACCESS_KEY: $creds_json" 1>&2
+        return "$rv"
+    fi
+    AWS_SESSION_TOKEN=$(echo "$creds_json" | jq --exit-status --raw-output .Credentials.SessionToken)
+    rv="$?"
+    if [[ $rv -ne 0 || ! $AWS_SESSION_TOKEN ]]; then
+        echo "$pkg: failed to parse output for AWS_SESSION_TOKEN: $creds_json" 1>&2
+        return "$rv"
+    fi
+
+    cat <<ENV
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
+ENV
 }
