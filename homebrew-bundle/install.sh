e84ec5d4f04f63236239b72e56804e26585aec03
Trevor Whitney <trevorjwhitney@gmail.com>
2020-11-17 15:39:21 -0700

    add yabai and skhd

--- homebrew-bundle/install.sh
+++ homebrew-bundle/install.sh
@@ -4,3 +4,10 @@ brewfile_directory=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 brew tap homebrew/bundle
 
 cd "$brewfile_directory" && brew bundle -v
+if [ ! `pgrep yabai` ]; then
+  brew services start yabai
+fi
+
+if [ ! `pgrep skhd` ]; then
+  brew services start skhd
+fi
