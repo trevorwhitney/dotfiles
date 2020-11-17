4f9e57d5478e7305e3d0afb38759e6b042498a05
Trevor Whitney <trevorjwhitney@gmail.com>
2020-11-17 15:39:21 -0700

    add yabai and skhd

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -9,5 +9,7 @@ create_link "$current_dir/ctags"
 create_link "$current_dir/dircolors"
 create_link "$current_dir/gitignore_global"
 create_link "$current_dir/git_template"
+create_link "$current_dir/yabairc"
+create_link "$current_dir/skhdrc"
 
 copy_file "$current_dir/gitconfig"
