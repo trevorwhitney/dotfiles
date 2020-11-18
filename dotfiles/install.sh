b2ea6e03ab16858e4b35801463caecf0c56bd807
Trevor Whitney <trevorjwhitney@gmail.com>
2020-11-18 13:45:29 -0700

    clean up yabai hotkeys, at limelightrc

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -11,5 +11,6 @@ create_link "$current_dir/gitignore_global"
 create_link "$current_dir/git_template"
 create_link "$current_dir/yabairc"
 create_link "$current_dir/skhdrc"
+create_link "$current_dir/limelightrc"
 
 copy_file "$current_dir/gitconfig"
