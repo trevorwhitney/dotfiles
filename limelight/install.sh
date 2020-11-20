7c62f998ae9c815808605a963233043408fc696c
Trevor Whitney <trevorjwhitney@gmail.com>
2020-11-20 09:23:59 -0700

    fix limelight config linking

--- limelight/install.sh
+++ limelight/install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-current_dir=$(cd $(dirname "$0") && pwd)
+current_dir=$(cd "$(dirname "$0")" && pwd)
 dot_files_dir=$(cd "$current_dir/.." && pwd)
 
 echo "Installing limelight"
 git clone git@github.com:koekeishiya/limelight.git "$HOME/workspace/limelight"
-cd "$HOME/workspace/limelight" || exit 1
+pushd "$HOME/workspace/limelight" || exit 1
 make
 ln -s "$HOME/workspace/limelight/bin/limelight" /usr/local/bin/limelight
+popd || exit 1
 
 echo "Linking limelight config"
 source "$dot_files_dir/lib.sh"
