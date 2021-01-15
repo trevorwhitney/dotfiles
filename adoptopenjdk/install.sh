5c98f9ae08ea00aaa4b56713086fdb2d1c31603a
Trevor Whitney <trevorjwhitney@gmail.com>
2021-01-14 21:11:08 -0700

    rename blacklist -> exclude, and support local exclude

--- adoptopenjdk/install.sh
+++ adoptopenjdk/install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 codename="$(cat /etc/os-release | grep UBUNTU_CODENAME | cut -d = -f 2)"
 wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb ${codename} main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list
+
+if [[ ! -e /etc/apt/sources.list.d/adoptopenjdk.list ]]; then
+  echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb ${codename} main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list
+fi
 sudo apt update
 sudo apt-get install -y adoptopenjdk-11-hotspot adoptopenjdk-14-hotspot
