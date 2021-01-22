a4fcd94f7a6f4b7fe04ec4f9cdaddc4eadb672cf
Trevor Whitney <trevorjwhitney@gmail.com>
2021-01-21 20:32:42 -0700

    install 2 latest go versions

--- go-linux/install.sh
+++ go-linux/install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 tmp_file="$(mktemp)"
-echo tmp_file
 wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz -O "${tmp_file}"
 
 tar -C /usr/local -xzf "${tmp_file}"
+mv /usr/local/go /usr/local/go@1.15
+
+tmp_file="$(mktemp)"
+wget https://golang.org/dl/go1.14.14.linux-amd64.tar.gz -O "${tmp_file}"
+tar -C /usr/local -xzf "${tmp_file}"
+mv /usr/local/go /usr/local/go@1.14
