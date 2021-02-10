eed27c0b50afa742051ba277531aa242933bde53
Trevor Whitney <trevorjwhitney@gmail.com>
2021-02-09 21:38:02 -0700

    a few changes on a new system
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- docker-linux/install.sh
+++ docker-linux/install.sh
@@ -3,8 +3,10 @@
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+if [[ ! -e /etc/apt/sources.list.d/docker.list ]]; then
+  echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+fi
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
