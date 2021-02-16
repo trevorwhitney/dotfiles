e408a32d277ffea60b069e596284ecc7f8f63b21
Trevor Whitney <trevorjwhitney@gmail.com>
2021-02-16 14:21:55 -0700

    finally get gruvbox looking good
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- gogh/install.sh
+++ gogh/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+current_dir="$(cd "$(dirname "$0")" && pwd)"
 
 mkdir -p "$HOME/.local/src"
 pushd "$HOME/.local/src" || exit 1
@@ -9,10 +10,11 @@ pushd "$HOME/.local/src" || exit 1
   pushd gogh/themes || exit 1
     # necessary on ubuntu
     export TERMINAL=gnome-terminal
+    ln -sf $current_dir/gruvbox-hard.sh
 
     # install themes
     ./solarized-light.sh
-    ./gruvbox.sh
+    ./gruvbox-hard.sh
     ./tomorrow.sh
   popd || exit 1
 popd || exit 1
