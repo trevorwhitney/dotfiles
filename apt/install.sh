c854e0ceec86c1a401871416b6d901fae566495a
Trevor Whitney <trevorjwhitney@gmail.com>
2021-02-16 06:58:55 -0700

    apt-get uses better repositories it seems
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- apt/install.sh
+++ apt/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-
-sudo apt update
-
 current_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+sudo apt-get update
+
 packages=$(cat $current_dir/packages | grep -v '^#')
-sudo apt install -y ${packages}
+sudo apt-get install -y ${packages}
