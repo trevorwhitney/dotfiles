563af5379f9098aab68e9dc1acaa456dd2ffea0b
Trevor Whitney <trevorjwhitney@gmail.com>
2021-02-19 12:12:25 -0700

    remove custom tmux mouse state
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -35,5 +35,3 @@ create_custom_zsh_link k3d-completion.zsh
 create_custom_zsh_link kubectl-completion.zsh
 create_custom_zsh_link themes/philips.zsh-theme
 create_custom_zsh_link themes/agnoster-gruvbox.zsh-theme
-
-ln -sf "$current_dir/tmux-mouse-state.sh" /usr/local/bin/tmux-mouse-state
--- oh-my-zsh/tmux-mouse-state.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-[[ "$(tmux show-options -gv mouse)" == "on" ]] && echo "🐁"
