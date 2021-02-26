a6d84cb922e34825afcaba3c6c9e34ec27088555
Trevor Whitney <trevorjwhitney@gmail.com>
2021-02-26 13:40:31 -0700

    install clipit
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

---
+++ clipit/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+current_dir=$(cd $(dirname $0) && pwd)
+
+tmp=$(mktemp -d)
+version=$(cat $current_dir/version)
+
+pushd ${tmp} > /dev/null || exit 1
+  curl -LkO "https://github.com/CristianHenzel/ClipIt/releases/download/v$version/clipit_${version}_amd64.deb"
+  curl -LkO "https://github.com/CristianHenzel/ClipIt/releases/download/v$version/clipit_${version}_amd64_indicator.deb"
+  sudo dpkg -i ./clipit_${version}_amd64.deb
+  sudo dpkg -i ./clipit_${version}_amd64_indicator.deb
+popd > /dev/null || exit 1
+
+rm -rf ${tmp}
---
+++ clipit/verify-install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+command -v clipit > /dev/null
+clipit --help > /dev/null
