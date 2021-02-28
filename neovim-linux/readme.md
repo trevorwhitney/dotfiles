bbafcd41e30b8b88ba79114b4d4c0348379437ef
Trevor Whitney <trevorjwhitney@gmail.com>
2021-02-28 16:03:25 -0700

    add coc for nvim

---
+++ neovim-linux/readme.md
@@ -0,0 +1,4 @@
+Post installation run:
+```
+:CocInstall coc-json coc-git coc-go coc-sql coc-yaml coc-yank coc-vimlsp coc-tsserver coc-spell-checker coc-snippets coc-sh coc-highlight coc-markdownlint
+```
--- vim-config/verify-install.sh
+++ vim-config/verify-install.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 command -v vim > /dev/null
+test -e $HOME/.vim/vimrc.config
+test -e $HOME/.vim/vimrc.bundles
 
-vim_plugs_desired=$(cat ~/.vim/vimrc.bundles | wc -l)
-vim_plugs_installed=$(ls -ld ~/.vim/plugged/* | wc -l)
-echo "Vim plugs installed: ${vim_plugs_installed}/${vim_plugs_desired}"
-test -e ~/.vim/vimrc.bundles && \
-  test ${vim_plugs_desired} -eq ${vim_plugs_installed}
