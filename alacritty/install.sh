3e18508f19718cbcc74832a37ca0dc8b7adb77f8
Trevor Whitney <trevorjwhitney@gmail.com>
2021-03-01 16:26:18 -0700

    add alacritty
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

---
+++ alacritty/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+current_dir="$(cd $(dirname "$0") && pwd)"
+
+
+create_alacritty_link() {
+  file_name="$(basename "$1" | sed "s/^\.//g")"
+  if [[ -h "$HOME/.config/alacritty/$file_name" ]] || [[ -e "$HOME/.config/alacritty/$file_name" ]]; then
+    rm -rf "$HOME/.config/alacritty/$file_name"
+  fi
+
+  ln -sf "$1" "$HOME/.config/alacritty/$file_name"
+}
+
+create_alacritty_link "$current_dir/alacritty.yml"
+create_alacritty_link "$current_dir/gruvbox.yml"
---
+++ alacritty/verify-install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+test -e "$HOME/.config/alacritty/alacritty.yml"
+test -e "$HOME/.config/alacritty/gruvbox.yml"
