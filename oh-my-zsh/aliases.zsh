8c2ca004e61430d99a28ad640875d21f3306025c
Trevor Whitney <trevorjwhitney@gmail.com>
2021-03-08 09:30:00 -0700

    remove bash-it
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -5,8 +5,20 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
 #Git
-alias gpr="git pull --rebase"
-alias gpp="git pull --rebase && git push"
+export GIT_DUET_GLOBAL=true
+export GIT_DUET_ROTATE_AUTHOR=1
+
+alias duet='git duet --global'
+alias solo='git solo --global'
+
+alias g='git'
+alias gpr='git pull --rebase'
+alias gpp='git pull --rebase && git push'
+alias gs='git status'
+alias gst='git status'
+
+alias gap="git add -p"
+alias gdf="git diff --color | diff-so-fancy"
 
 #Gradle
 alias g="gradle"
