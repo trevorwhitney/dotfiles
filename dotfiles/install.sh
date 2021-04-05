1e39a656d7481ae1237949d37a2cbc914aa93148
Trevor Whitney <trevorjwhitney@gmail.com>
2021-04-05 11:24:23 -0600

    make k9s folder

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -13,4 +13,5 @@ create_link "$current_dir/git_template"
 copy_file "$current_dir/gitconfig"
 
 create_config_link "k9s" "$current_dir/gruvbox-light-skin.yml"
+mkdir -p "$HOME/.k9s"
 ln -sf "$HOME/.config/k9s/gruvbox-light-skin.yml" "$HOME/.k9s/skin.yml"
