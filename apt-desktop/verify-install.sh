c5c276fc5df604171e6c8595d802f44f6bcfbb2e
Trevor Whitney <trevorjwhitney@gmail.com>
2021-04-07 14:44:53 -0600

    split apt between essential and desktop apps
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

---
+++ apt-desktop/verify-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function verify_package() {
+  package=${1}
+  dpkg -s "$package" > /dev/null
+}
+
+current_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+packages=$(cat "$current_dir/packages" | grep -v '^#')
+for package in $packages; do
+  if ! verify_package $package; then
+    echo "Failed to find apt package $package"
+    exit 1;
+  fi
+done
