db5cc32f383274ac082ce288668b6528b38702c9
Trevor Whitney <trevorjwhitney@gmail.com>
2021-04-07 20:58:35 +0000

    make configs a little more remote friendly
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- jetbrains-mono/install.sh
+++ jetbrains-mono/install.sh
@@ -4,7 +4,8 @@ temp="$(mktemp -d)"
 
 pushd "$temp" > /dev/null || exit 1
   curl -LO https://download.jetbrains.com/fonts/JetBrainsMono-2.225.zip
-  unzip -d "$HOME/.local/share/" JetBrainsMono-2.225.zip
+  unzip JetBrainsMono-2.225.zip "fonts/ttf/*"
+  mv fonts/ttf/* ~/.local/share/fonts
 popd
 
 sudo fc-cache -f -v
