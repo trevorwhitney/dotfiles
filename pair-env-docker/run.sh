f7d751db96ec1e5ddd0b1ad1ac691341fe81db20
Trevor Whitney <trevorjwhitney@gmail.com>
2021-04-14 10:03:22 -0600

    update to work on cloud k8s
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- pair-env-docker/run.sh
+++ pair-env-docker/run.sh
@@ -4,8 +4,12 @@ set -euo pipefail
 
 set -x
 
+# the purge can fail when /etc/ssh is a mounted volume in k8s
+# so try it to delete what we can, but don't stop the script if it fails
+set +e
 # reinstall openssh if no config provided
 test -e /etc/ssh/sshd_config || { dpkg --purge openssh-server; apt-get install -y openssh-server; }
+set -e
 
 # home directory won't exsist on first boot since the user was created during docker file build
 # before the volume is mounted
