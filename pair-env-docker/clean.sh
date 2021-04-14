5bd329e1d0c1c682efacdf31fa0f87809361b45d
Trevor Whitney <trevorjwhitney@gmail.com>
2021-04-13 19:59:10 -0600

    got dev docker image working
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- pair-env-docker/clean.sh
+++ pair-env-docker/clean.sh
@@ -2,6 +2,6 @@
 
 current_dir=$(cd "$(dirname "$0")" && pwd)
 
-pushd $current_dir > /dev/null || exit 1
-  rm -rf .git $(< cat dotexclude)
+pushd "$current_dir/.." > /dev/null || exit 1
+  rm -rf .git .gitignore .gitmodules .idea $(< pair-env-docker/dotexclude)
 popd > /dev/null || exit 1
