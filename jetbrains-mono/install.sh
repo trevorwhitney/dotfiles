3a55983c8ce37c10cdfff16867f314ec49c7239f
Trevor Whitney <trevorjwhitney@gmail.com>
2021-04-26 16:41:58 -0600

    reinstall on chromebook
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- jetbrains-mono/install.sh
+++ jetbrains-mono/install.sh
@@ -5,6 +5,7 @@ temp="$(mktemp -d)"
 pushd "$temp" > /dev/null || exit 1
   curl -LO https://download.jetbrains.com/fonts/JetBrainsMono-2.225.zip
   unzip JetBrainsMono-2.225.zip "fonts/ttf/*"
+  mkdir -p ~/.local/share/fonts
   mv fonts/ttf/* ~/.local/share/fonts
 popd
 
--- powerline-fonts/install.sh
+++ powerline-fonts/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 mkdir -p ~/workspace
-cd ~/workspace
-git clone https://github.com/powerline/fonts.git
+pushd ~/workspace > /dev/null || exit 1
+  git clone https://github.com/powerline/fonts.git
+  pushd fonts > /dev/null || exit 1
+    bash install.sh
+  popd > /dev/null || exit 1
 
-cd fonts
-bash install.sh
+  rm -rf fonts
+popd > /dev/null || exit 1
