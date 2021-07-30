0370add3282e0536b92c81aeb53b6c64adff0479
Trevor Whitney <trevorjwhitney@gmail.com>
2021-07-30 16:44:48 -0600

    install nerd fonts with jb mono
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- jetbrains-mono/install.sh
+++ jetbrains-mono/install.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env zsh
 
-temp="$(mktemp -d)"
+jbTemp="$(mktemp -d)"
 
-pushd "$temp" > /dev/null || exit 1
+pushd "$jbTemp" > /dev/null || exit 1
   curl -LO https://download.jetbrains.com/fonts/JetBrainsMono-2.225.zip
   unzip JetBrainsMono-2.225.zip "fonts/ttf/*"
   mkdir -p ~/.local/share/fonts
   mv fonts/ttf/* ~/.local/share/fonts
 popd
 
-sudo fc-cache -f -v
+rm -rf $jbTemp
 
-rm -rf $temp
+nerdTemp=$(mktemp -d)
+pushd $nerdTemp > /dev/null || exit 1
+  git clone git@github.com:ryanoasis/nerd-fonts.git
+  pushd nerd-fonts > /dev/null || exit 1
+    ./install.sh JetBrainsMono
+  popd > /dev/null || exit 1
+popd > /dev/null || exit 1
+
+rm -rf $nerdTemp
+
+sudo fc-cache -f -v
--- jetbrains-mono/verify-install.sh
+++ jetbrains-mono/verify-install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
 fc-list | grep JetBrainsMono > /dev/null
+fc-list | grep Nerd > /dev/null
