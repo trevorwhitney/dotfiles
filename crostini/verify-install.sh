c15db7e977e6946196e022dea10540baf717c250
Trevor Whitney <trevorjwhitney@gmail.com>
2021-09-20 20:13:57 -0600

    add better alacritty desktop file for crostini
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- crostini/verify-install.sh
+++ crostini/verify-install.sh
@@ -4,5 +4,8 @@ set -e
 
 test -e "$HOME/.config/alacritty/alacritty-crostini.yml"
 test -e "$HOME/.oh-my-zsh/custom/crostini.zsh"
+test -e "/usr/share/applications/alacritty.desktop"
+test -e "$HOME/.local/share/icons/alacritty.png"
+test -e "$HOME/.local/share/icons/alacritty.svg"
 
 command -v alacritty > /dev/null
