0119c84ae75cedb00ff846faced59058bbffe0e2
Trevor Whitney <trevorjwhitney@gmail.com>
2021-09-20 20:13:57 -0600

    add better alacritty desktop file for crostini
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- crostini/install.sh
+++ crostini/install.sh
@@ -13,6 +13,9 @@ create_alacritty_link "$current_dir/alacritty-crostini.yml"
 
 ln -sf "$HOME/.config/alacritty/alacritty-crostini.yml" "$HOME/.config/alacritty/alacritty.yml"
 sudo ln -sf "$current_dir/alacritty.desktop" /usr/share/applications/alacritty.desktop
+mkdir -p "$HOME/.local/share/icons"
+sudo ln -sf "$current_dir/alacritty.png" "$HOME/.local/share/icons/alacritty.png"
+sudo ln -sf "$current_dir/alacritty.svg" "$HOME/.local/share/icons/alacritty.svg"
 
 # Move to Debian SID and install Tailscale
 sudo ln -sf "$current_dir/sources.list" /etc/apt/sources.list
