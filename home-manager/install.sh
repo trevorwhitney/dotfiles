ad24ac2e9867aed145a2e8b3bc2d45a56045fcb6
Trevor Whitney <trevorjwhitney@gmail.com>
2021-10-04 21:36:35 -0600

    add home-manager dir for playing around with nix stuff
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

---
+++ home-manager/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+current_dir=$(cd "$(dirname $0)" && pwd)
+
+if [[ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]]; then
+  . $HOME/.nix-profile/etc/profile.d/nix.sh
+fi
+
+test ! `command -v nix` && curl -L https://nixos.org/nix/install | sh
+if [[ ! `command -v home-manager` ]]; then
+  . $HOME/.nix-profile/etc/profile.d/nix.sh
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+  nix-channel --update
+  nix-shell '<home-manager>' -A install
+fi
+
+if [[ ! -e "$HOME/.config/nixpkgs" ]]; then
+  ln -s $current_dir/nixpkgs "$HOME/.config/nixpkgs"
+fi
