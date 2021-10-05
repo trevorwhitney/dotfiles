a0daf95efdb4ec24ad4a100467fac21fb2f6b7eb
Trevor Whitney <trevorjwhitney@gmail.com>
2021-10-05 09:55:50 -0600

    copy kns and ktx scripts into dots
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- kubectl-linux/install.sh
+++ kubectl-linux/install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 
+current_dir=$(cd "$(dirname $0)" && pwd)
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
 
-curl https://raw.githubusercontent.com/trevorwhitney/kns/master/bin/kns -o "$HOME/.local/bin/kns"  && chmod +x $_
-curl https://raw.githubusercontent.com/trevorwhitney/kns/master/bin/ktx -o "$HOME/.local/bin/ktx"  && chmod +x $_
+# kns and ktx from https://github.com/blendle/kns
+ln -s "$current_dir/kns" "$HOME/.local/bin/kns"
+ln -s "$current_dir/ktx" "$HOME/.local/bin/ktx"
