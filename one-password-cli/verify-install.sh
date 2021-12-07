1a49db8311a351fab47932a32e37b5ef91d911fb
Trevor Whitney <trevorjwhitney@gmail.com>
2021-12-07 14:56:10 -0700

    update one password version

--- one-password-cli/verify-install.sh
+++ one-password-cli/verify-install.sh
@@ -2,3 +2,9 @@
 
 command -v op > /dev/null
 op --version > /dev/null
+
+
+current_dir="$(cd $(dirname "$0") && pwd)"
+current_version="$(cat "$current_dir/version" | sed 's/v//')"
+
+if [[ "v$current_version" != "v$(op --version)" ]]; then exit 1; fi
