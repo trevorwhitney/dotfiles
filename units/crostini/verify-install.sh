a8e10532688b947576cd0bcfce06e5c1f1365c65
Trevor Whitney <trevorjwhitney@gmail.com>
2021-12-08 12:38:07 -0700

    remove oh-my-zsh references

--- units/crostini/verify-install.sh
+++ units/crostini/verify-install.sh
@@ -3,7 +3,8 @@
 set -e
 
 test -e "$HOME/.config/alacritty/alacritty-crostini.yml"
-test -e "$HOME/.oh-my-zsh/custom/crostini.zsh"
+# TODO: figure out how to get this into home-manager
+# test -e "$HOME/.oh-my-zsh/custom/crostini.zsh"
 test -e "/usr/share/applications/alacritty.desktop"
 test -e "/usr/share/applications/firefox.desktop"
 test -e "$HOME/.local/share/icons/alacritty.png"
