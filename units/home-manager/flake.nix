beaa8d7853c0c18a718e84e1e31462f329ceecf7
Trevor Whitney <trevorjwhitney@gmail.com>
2022-05-31 14:44:32 -0600

    Add LOCALE_ARCHIVE env var

--- units/home-manager/flake.nix
+++ units/home-manager/flake.nix
@@ -20,86 +20,109 @@
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, flake-utils
-    , secrets, mosh, neovim-nightly-overlay, ... }: {
-      homeConfigurations = let
-        overlay-unstable = final: prev: {
-          unstable = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux;
-        };
-
-        overlay-neovim = neovim-nightly-overlay.overlay;
-        commonConfig = system: {
-          inherit system;
-          # TODO: can you use builtins here?
-          homeDirectory = "/home/twhitney";
-          username = "twhitney";
-        };
-
-        commonImports = { system, config, pkgs, lib }: [
-          ./nixpkgs/modules/common.nix
-          ./nixpkgs/modules/bash.nix
-          ./nixpkgs/modules/git.nix
-          (import ./nixpkgs/modules/tmux.nix {
-            inherit config pkgs lib;
-            # TODO: is there a way to use a builtin to get the current system
-            nixpkgs = import nixpkgs { inherit system; };
-          })
-          ./nixpkgs/modules/zsh.nix
-        ];
-
-        commonPackages = [ mosh.defaultPackage.x86_64-linux ];
-      in {
-        "twhitney@cerebral" = let system = "x86_64-linux";
-        in self.inputs.home-manager.lib.homeManagerConfiguration
-        (commonConfig system // {
-          configuration = { config, pkgs, lib, ... }: {
-            nixpkgs.overlays = [ overlay-unstable overlay-neovim ];
-            nixpkgs.config = {
-              allowUnfree = true;
-              allowBroken = true;
-            };
-
-            imports = [
-              ./nixpkgs/modules/media.nix
-              ./nixpkgs/modules/spotify.nix
-              (import ./nixpkgs/modules/neovim.nix {
-                inherit config pkgs lib;
-                withLspSupport = true;
-              })
-            ] ++ commonImports { inherit config pkgs lib system; };
-
-            home.packages = commonPackages;
-
-            programs.git.includes =
-              [{ path = "${inputs.secrets.defaultPackage.${system}}/git"; }];
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , nixpkgs-unstable
+    , home-manager
+    , flake-utils
+    , secrets
+    , mosh
+    , neovim-nightly-overlay
+    , ...
+    }: {
+      homeConfigurations =
+        let
+          overlay-unstable = final: prev: {
+            unstable = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux;
           };
-        });
 
-        "twhitney@penguin" = let system = "x86_64-linux";
-        in self.inputs.home-manager.lib.homeManagerConfiguration
-        (commonConfig system // {
-          configuration = { config, pkgs, lib, ... }: {
-            nixpkgs.overlays = [ overlay-unstable overlay-neovim ];
-            nixpkgs.config = {
-              allowUnfree = true;
-              allowBroken = true;
-            };
-
-            imports = [
-              (import ./nixpkgs/modules/neovim.nix {
-                inherit config pkgs lib;
-                withLspSupport = false;
-              })
-            ] ++ commonImports { inherit config pkgs lib system; };
-
-            home.packages = commonPackages;
-
-            programs.git.includes =
-              [{ path = "${inputs.secrets.defaultPackage.${system}}/git"; }];
-
-            programs.zsh.sessionVariables = { GPG_TTY = "$(tty)"; };
+          overlay-neovim = neovim-nightly-overlay.overlay;
+          commonConfig = system: {
+            inherit system;
+            # TODO: can you use builtins here?
+            homeDirectory = "/home/twhitney";
+            username = "twhitney";
           };
-        });
-      };
+
+          commonImports = { system, config, pkgs, lib }: [
+            ./nixpkgs/modules/common.nix
+            ./nixpkgs/modules/bash.nix
+            ./nixpkgs/modules/git.nix
+            (import ./nixpkgs/modules/tmux.nix {
+              inherit config pkgs lib;
+              # TODO: is there a way to use a builtin to get the current system
+              nixpkgs = import nixpkgs { inherit system; };
+            })
+            ./nixpkgs/modules/zsh.nix
+          ];
+
+          commonPackages = [ mosh.defaultPackage.x86_64-linux ];
+        in
+        {
+          "twhitney@cerebral" =
+            let system = "x86_64-linux";
+            in
+            self.inputs.home-manager.lib.homeManagerConfiguration
+              (commonConfig system // {
+                configuration = { config, pkgs, lib, ... }: {
+                  nixpkgs.overlays = [ overlay-unstable overlay-neovim ];
+                  nixpkgs.config = {
+                    allowUnfree = true;
+                    allowBroken = true;
+                  };
+
+                  imports = [
+                    ./nixpkgs/modules/media.nix
+                    ./nixpkgs/modules/spotify.nix
+                    (import ./nixpkgs/modules/neovim.nix {
+                      inherit config pkgs lib;
+                      withLspSupport = true;
+                    })
+                  ] ++ commonImports { inherit config pkgs lib system; };
+
+                  home.packages = commonPackages;
+
+                  programs.git.includes =
+                    [{ path = "${inputs.secrets.defaultPackage.${system}}/git"; }];
+
+                  programs.zsh.sessionVariables = {
+                    GPG_TTY = "$(tty)";
+                    LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive";
+                  };
+                };
+              });
+
+          "twhitney@penguin" =
+            let system = "x86_64-linux";
+            in
+            self.inputs.home-manager.lib.homeManagerConfiguration
+              (commonConfig system // {
+                configuration = { config, pkgs, lib, ... }: {
+                  nixpkgs.overlays = [ overlay-unstable overlay-neovim ];
+                  nixpkgs.config = {
+                    allowUnfree = true;
+                    allowBroken = true;
+                  };
+
+                  imports = [
+                    (import ./nixpkgs/modules/neovim.nix {
+                      inherit config pkgs lib;
+                      withLspSupport = false;
+                    })
+                  ] ++ commonImports { inherit config pkgs lib system; };
+
+                  home.packages = commonPackages;
+
+                  programs.git.includes =
+                    [{ path = "${inputs.secrets.defaultPackage.${system}}/git"; }];
+
+                  programs.zsh.sessionVariables = {
+                    GPG_TTY = "$(tty)";
+                    LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive";
+                  };
+                };
+              });
+        };
     };
 }
