777192f0bb626a6b93d1b028c0bd7e78b7cf6bad
Trevor Whitney <trevorjwhitney@gmail.com>
2022-06-01 08:53:09 -0600

    Convert mosh flake to use overrideAttrs

--- units/home-manager/flakes/mosh/flake.nix
+++ units/home-manager/flakes/mosh/flake.nix
@@ -7,7 +7,13 @@
   outputs = { self, nixpkgs, flake-utils }:
     {
       overlay = (final: prev: {
-        mosh = prev.callPackage ./default.nix { pkgs = prev; };
+        mosh = prev.mosh.overrideAttrs (oldAttrs: rec {
+          buildInputs = oldAttrs.buildInputs ++ (with prev; [ glibcLocales ]);
+          postInstall = with prev; ''
+            wrapProgram $out/bin/mosh --prefix PERL5LIB : $PERL5LIB;
+            wrapProgram $out/bin/mosh-server --set LOCALE_ARCHIVE ${glibcLocales}/lib/locale/locale-archive;
+          '';
+        });
       });
     } // (flake-utils.lib.eachDefaultSystem (system:
       let
@@ -15,7 +21,8 @@
           inherit system;
           overlays = [ self.overlay ];
         };
-      in {
+      in
+      {
         defaultPackage = pkgs.mosh;
         devShell = import ./shell.nix { inherit pkgs; };
         packages = { mosh = pkgs.mosh; };
