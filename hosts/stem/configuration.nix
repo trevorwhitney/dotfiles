681abd892934dadf78444cbeeba5a0c3986da081
Trevor Whitney <trevorjwhitney@gmail.com>
2022-06-07 16:44:23 -0600

    Enable auto-login and disable sleep for stem

--- hosts/stem/configuration.nix
+++ hosts/stem/configuration.nix
@@ -5,10 +5,10 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -20,20 +20,14 @@
     experimental-features = nix-command flakes
   '';
 
-  # allow unfree software (needed for plex)
-  nixpkgs.config.allowUnfree = true;
-
   networking.hostName = "stem"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable =
+    true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "America/Denver";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
@@ -45,97 +39,99 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+
+  # Enable gnome
+  services.xserver.displayManager.gdm = {
+    enable = true;
+    autoSuspend = false;
+  };
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable plex
-  services.plex = {
-    enable = true;
-    openFirewall = true;
+  # Auto-login to gnome
+  services.xserver.displayManager.gdm.settings = {
+    daemon = {
+      AutomaticLoginEnable = true;
+      AutomaticLogin = twhitney;
+    };
   };
+
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "caps:ctrl";
 
   # Enable sound.
   sound.enable = true;
   # hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.twhitney = {
-     isNormalUser = true;
-     home = "/home/twhitney";
-     extraGroups = [ "wheel" "networkmanager" "docker" "plex" ];
-     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBSeuF+NMj8sKD8kWuahlSasaPzHzT5Jhip+Y+EAcfEv trevorjwhitney@gmail.com"];
-   };
+  users.users.twhitney = {
+    isNormalUser = true;
+    home = "/home/twhitney";
+    extraGroups = [ "wheel" "networkmanager" "docker" "plex" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBSeuF+NMj8sKD8kWuahlSasaPzHzT5Jhip+Y+EAcfEv trevorjwhitney@gmail.com"
+    ];
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
+    docker
+    firefox
+    git
+    gnome3.gnome-tweaks
     gnomeExtensions.appindicator
     gnomeExtensions.pop-shell
-    gnome3.gnome-tweaks
-    nerdfonts
-    kubectl
-    docker
-    kube3d
+    k9s
     kind
-    git
-    firefox
+    kube3d
+    kubectl
+    nerdfonts
+    vim
+    wget
   ];
 
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-photos
-    gnome-tour
-  ]) ++ (with pkgs.gnome; [
-    gnome-music
-    epiphany # web browser
-    geary # email reader
-    evince # document viewer
-    totem # video player
-    tali # poker game
-    iagno # go game
-    hitori # sudoku game
+  environment.gnome.excludePackages = (with pkgs; [ gnome-photos gnome-tour ])
+    ++ (with pkgs.gnome; [
     atomix # puzzle game
+    epiphany # web browser
+    evince # document viewer
+    geary # email reader
+    gnome-music
+    hitori # sudoku game
+    iagno # go game
+    tali # poker game
+    totem # video player
   ]);
 
   services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Enable plex
+  # allow unfree software (needed for plex)
+  nixpkgs.config.allowUnfree = true;
+
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ 6443 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -145,12 +141,6 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.05"; # Did you read the comment?
 
-  # enable kubernetes
-  # services.kubernetes.roles = [ "master" "node" ];
-  # services.kubernetes.masterAddress = "stem";
-
   # enable docker
   virtualisation.docker.enable = true;
-  # virtualisation.libvirtd.enable = true;
 }
-
