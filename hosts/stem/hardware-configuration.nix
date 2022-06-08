b2bd29e98fe6b7bfce58a18d0cf311f9185c8fc6
Trevor Whitney <trevorjwhitney@gmail.com>
2022-06-08 11:14:50 -0600

    Add wd and seagate encrypted drives

--- hosts/stem/hardware-configuration.nix
+++ hosts/stem/hardware-configuration.nix
@@ -42,6 +42,26 @@
       fsType = "vfat";
     };
 
+  fileSystems."/mnt/seagate" =
+    { device = "/dev/disk/by-uuid/3466bf26-59db-471f-85f9-610fd8807c1a";
+      fsType = "ext4";
+      encrypted = {
+        enable = true;
+        keyFile = "/etc/luks-keys/seagate_secret_key";
+        label = "seagate_crypt";
+      };
+    };
+
+  fileSystems."/mnt/wd" =
+    { device = "/dev/disk/by-uuid/a0ac0856-8d02-4c96-bc6d-4d990e6ef67f";
+      fsType = "ext4";
+      encrypted = {
+        enable = true;
+        keyFile = "/etc/luks-keys/wd_secret_key";
+        label = "wd_crypt";
+      };
+    };
+
   swapDevices =
     [ { device = "/dev/disk/by-uuid/19e4f435-3a75-497f-9e5b-eba1409850dd"; }
     ];
