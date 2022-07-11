f892f03ab221bf43beed94f78a80c88e7b68769d
Trevor Whitney <trevorjwhitney@gmail.com>
2022-07-11 15:02:15 -0600

    Make bubbles part of dotfiles flake

--- nix/home-manager/i3.nix
+++ nix/home-manager/i3.nix
@@ -3,4 +3,5 @@ let inherit (pkgs) dotfiles;
 in
 {
   xdg.configFile."i3".source = "${dotfiles}/config/i3";
+  home.file.".local/share/backgrounds/solarized-bubbles.pngs".source = "${dotfiles}/wallpapers/solarized-bubbles.png";
 }
