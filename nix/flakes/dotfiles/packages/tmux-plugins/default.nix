914404f5ef875728807f532c581116b9a2dde17d
Trevor Whitney <trevorjwhitney@gmail.com>
2022-09-23 11:08:23 -0600

    Bump tmux-lib version

--- nix/flakes/dotfiles/packages/tmux-plugins/default.nix
+++ nix/flakes/dotfiles/packages/tmux-plugins/default.nix
@@ -49,7 +49,7 @@ in
       owner = "trevorwhitney";
       repo = "tw-tmux-lib";
       rev = "main";
-      sha256 = "1kp51wi22f8cc0zi6b0pqah910n0ypy3rbrnadk3dlc7g1gxbrlc";
+      sha256 = "1977i745gbxmdf24ai2alp3aharlya53vfy7aibcj5ic00xb8x40";
     };
   };
 }
