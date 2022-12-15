e84eff85718c294087b956d3e4368fa6ca003073
Trevor Whitney <trevorjwhitney@gmail.com>
2022-12-15 16:02:24 -0700

    audacity and kdenlive stuff

--- units/cerebral/install.sh
+++ units/cerebral/install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 current_dir=$(cd "$(dirname $0)" && pwd)
 
+# Kdenlive and OBS Studio PPAs
+sudo add-apt-repository ppa:kdenlive/kdenlive-stable
+sudo apt-add-repository ppa:obsproject/obs-studio
+
 sudo apt-get update
 sudo apt-get install -y $(< $current_dir/packages)
--- units/crostini/install.sh
+++ units/crostini/install.sh
@@ -9,11 +9,4 @@ source "$dot_files_dir/lib.sh"
 
 # Move to Debian SID
 sudo ln -sf "$current_dir/sources.list" /etc/apt/sources.list
-curl -fsSL https://pkgs.tailscale.com/stable/debian/sid.gpg | sudo apt-key add -
-curl -fsSL https://pkgs.tailscale.com/stable/debian/sid.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-sudo apt-get update
-
-# firefox
-sudo apt update && sudo apt install -y firefox gnome-keyring
-mkdir -p $HOME/.local/share/applications/
-sudo ln -sf "$current_dir/firefox.desktop" /usr/share/applications/firefox.desktop
+sudo apt update && sudo apt install -y gnome-keyring
