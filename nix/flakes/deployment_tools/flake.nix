47138baaeda71b58161e1893b44746f6634f2cc9
Trevor Whitney <trevorjwhitney@gmail.com>
2023-01-10 11:07:46 -0700

    add deployment-tools secrets

--- nix/flakes/deployment_tools/flake.nix
+++ nix/flakes/deployment_tools/flake.nix
@@ -1,14 +1,25 @@
 {
   description = "Flake for working with Grafana deployment_tools repo";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs = {
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    #TODO: replace with https://github.com/ryantm/agenix
+    secrets.url =
+      "git+ssh://git@github.com/trevorwhitney/home-manager-secrets.git?ref=main&rev=f30db379c7b2621ade6ff8a3114f924d65514751";
+    secrets.inputs.nixpkgs.follows = "nixpkgs";
+    secrets.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, secrets }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
+            secrets.overlay
             (import ../../overlays/kubectl.nix { inherit system; })
             # rt
             (final: prev: {
@@ -16,7 +27,13 @@
                 name = "scripts";
                 pname = "scripts";
 
-                src = /home/twhitney/workspace/deployment_tools;
+                buildInputs = [ prev.openssh ];
+
+                src = builtins.fetchGit {
+                  # use ssh for private repo
+                  url = "ssh://git@github.com/grafana/deployment_tools";
+                  rev = "f6a4c18b00f86256f4b00780caf02b49ee2044c3";
+                };
 
                 configurePhase = "patchShebangs scripts";
 
@@ -43,7 +60,7 @@
           ];
 
           shellHook = ''
-            export GRAFANA_TEAM="loki"
+            source ${pkgs.secrets}/grafana/deployment-tools.sh
           '';
         };
       });
