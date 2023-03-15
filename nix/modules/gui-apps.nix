64013e212cd7b2721a75bc7f621be80003c44b1b
Trevor Whitney <trevorjwhitney@gmail.com>
2023-03-15 09:55:32 -0600

    add some cad programs

--- nix/modules/gui-apps.nix
+++ nix/modules/gui-apps.nix
@@ -3,13 +3,22 @@
     _1password-gui
     audacity
     davinci-resolve
+    freecad
     gimp
     google-chrome-beta
     insomnia
     kdenlive
+    librecad
+    qcad
     slack
     spotify
     vlc
+
+    # fix ODAFileConverter for wayland
+    (pkgs.writeShellScriptBin "ODAFileConverter" ''
+      #!${pkgs.stdenv.shell}
+      QT_QPA_PLATFORM=wayland "${pkgs.odafileconverter}/bin/ODAFileConverter" "$@"
+    '')
 
     # fix needed for screensharing
     # make sure to set enableWaylandShare = true
