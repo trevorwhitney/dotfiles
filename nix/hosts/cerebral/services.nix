aba84b0122ad23d222cce0c83019ba4df0446cae
Trevor Whitney <trevorjwhitney@gmail.com>
2023-07-27 15:14:47 -0600

    Improve plex nginx config

--- nix/hosts/cerebral/services.nix
+++ nix/hosts/cerebral/services.nix
@@ -133,12 +133,36 @@ in
       virtualHosts."plex.trevorwhitney.net" = {
         useACMEHost = "plex.trevorwhitney.net";
         forceSSL = true;
+        extraConfig = ''
+          gzip on;
+          gzip_vary on;
+          gzip_min_length 1000;
+          gzip_proxied any;
+          gzip_types text/plain text/css text/xml application/xml text/javascript application/x-javascript image/svg+xml;
+          gzip_disable "MSIE [1-6]\.";
+
+          # Forward real ip and host to Plex
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          #When using ngx_http_realip_module change $proxy_add_x_forwarded_for to '$http_x_forwarded_for,$realip_remote_addr'
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header Sec-WebSocket-Extensions $http_sec_websocket_extensions;
+          proxy_set_header Sec-WebSocket-Key $http_sec_websocket_key;
+          proxy_set_header Sec-WebSocket-Version $http_sec_websocket_version;
+
+          # Websockets
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "Upgrade";
+
+          # Buffering off send to the client as soon as the data is received from Plex.
+          proxy_redirect off;
+          proxy_buffering off;
+        '';
         locations = {
           "/" = {
-            proxyPass = "http://127.0.0.1:32400/web/";
-          };
-          "/web" = {
-            proxyPass = "http://127.0.0.1:32400/web";
+            proxyPass = "https://127.0.0.1:32400";
           };
         };
       };
