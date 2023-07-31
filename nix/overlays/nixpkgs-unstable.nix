15f172d04c2970ec0b4a617df76c16c80c3670e6
Trevor Whitney <trevorjwhitney@gmail.com>
2023-07-31 10:40:01 -0600

    Clean-up unstable overrides

--- nix/overlays/nixpkgs-unstable.nix
+++ nix/overlays/nixpkgs-unstable.nix
@@ -6,24 +6,13 @@ final: prev: rec {
     kitty
     kitty-themes
 
-    # TODO: sumneko was renamed to just lua-language server. Remove after 23.05 and switch to using stable.
-    lua-language-server
+    # neovim
+    neovim
+    neovim-unwrapped
 
     # GUI apps that I'd like to be on the latest version of
     _1password-gui
-    neovim
-    neovim-unwrapped
     gimp
     spotify
-    thunderbird
-
-    # Wayland screensharing fixed in latest Zoom
-    zoom-us;
-
-    # Things that enable screensharing need to come from stable
-    # wrapFirefox
-    # firefox-beta
-    # firefox-beta-bin;
-    # google-chrome-beta
-    # slack
+    thunderbird;
 }
