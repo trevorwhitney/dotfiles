604783504764efcfd8a88b3a59c3522c38ada860
Trevor Whitney <trevorjwhitney@gmail.com>
2023-09-18 16:23:47 -0600

    Improve 1password ssh-agent integration

--- nix/hosts/cerebral/1password.nix
+++ nix/hosts/cerebral/1password.nix
@@ -5,4 +5,14 @@
     enable = true;
     polkitPolicyOwners = [ "twhitney" ];
   };
+
+  # allow me to use 1password ssh agent without password prompt
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (subject.user == "twhitney" && 
+          action.id == "com.1password.1Password.unlock") {
+        return polkit.Result.YES;
+      }
+    });
+  '';
 }
