e25e2a0942e395501f941e5c319f15c11440e53e
Trevor Whitney <trevorjwhitney@gmail.com>
2023-10-09 15:54:53 -0600

    switch crostini to use 1pass for ssh keys

--- nix/home-manager/hosts/penguin.nix
+++ nix/home-manager/hosts/penguin.nix
@@ -24,6 +24,7 @@ in
         baseConfig
         config
         ../modules/tmux.nix
+        ../modules/1password.nix
         {
           programs.neovim = {
             withLspSupport = false;
@@ -37,44 +38,6 @@ in
               mosh-cerebral = ''
                 mosh --server ~/.nix-profile/bin/mosh-server twhitney@cerebral.trevorwhitney.net
               '';
-            };
-          };
-
-          # TODO: switch to 1password for all this stuff
-          systemd.user = with pkgs; {
-            sessionVariables = {
-              SSH_AUTH_SOCK = ''''${XDG_RUNTIME_DIR}/ssh-agent.socket'';
-            };
-            services.ssh-agent = {
-              Install.WantedBy = [ "default.target" ];
-              Unit.Description = "SSH key agent";
-              Service = {
-                Type = "simple";
-                Environment = "SSH_AUTH_SOCK=%t/ssh-agent.socket";
-                ExecStart = "${writers.writeBash "start-ssh-agent" ''
-                    if ! pgrep -u $USER ssh-agent > /dev/null; then
-                        ${pkgs.openssh}/bin/ssh-agent -D -a $SSH_AUTH_SOCK > ~/.ssh-agent-info
-                        ssh-agent > ~/.ssh-agent-info
-                    fi
-
-                    if [[ "$SSH_AGENT_PID" == "" ]]; then
-                        source ~/.ssh-agent-info
-                    fi
-
-                    # TODO: will likely need some sort of pinentry program to
-                    # save the password in between boots
-                    for KEY in $(ls $HOME/.ssh/id_ed25519* | grep -v \.pub); do
-                      ssh-add -q ''${KEY} </dev/null
-                    done
-                  ''}";
-                ExecStop = "${pkgs.writers.writeBash "stop-ssh-agent" ''
-                    if pgrep -u $USER ssh-agent > /dev/null; then
-                        source ~/.ssh-agent-info
-                    fi
-
-                    [ -z "$SSH_AGENT_PID" ] || eval "$(ssh-agent -k)"
-                  ''}";
-              };
             };
           };
         }
