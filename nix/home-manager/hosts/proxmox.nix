2ac29a52917527c96eb80eeaa4786dacb48a0b16
Trevor Whitney <trevorjwhitney@gmail.com>
2025-01-30 09:33:46 -0700

    fix proxmox config more

--- nix/home-manager/hosts/proxmox.nix
+++ nix/home-manager/hosts/proxmox.nix
@@ -1,36 +1,34 @@
 { pkgs, home-manager, username, ... }: {
-  "twhitney@proxmox" = home-manager.lib.homeManagerConfiguration rec {
+  "twhitney@proxmox" = home-manager.lib.homeManagerConfiguration {
     inherit pkgs;
-    home = {
-      inherit username;
-      homeDirectory = "/home/${username}";
-      stateVersion = "21.11";
-    };
+    modules = [
+      rec {
+        home = {
+          inherit username;
+          homeDirectory = "/home/${username}";
+          stateVersion = "21.11";
+        };
 
-    home.sessionVariables = {
-      EDITOR = "nvim";
-      XDG_CACHE_HOME = "${home.homeDirectory}/.cache";
-      XDG_CONFIG_HOME = "${home.homeDirectory}/.config";
-      XDG_DATA_HOME = "${home.homeDirectory}/.local/share";
-      XDG_STATE_HOME = "${home.homeDirectory}/.local/state";
-    };
+        home.sessionVariables = {
+          EDITOR = "nvim";
+          XDG_CACHE_HOME = "${home.homeDirectory}/.cache";
+          XDG_CONFIG_HOME = "${home.homeDirectory}/.config";
+          XDG_DATA_HOME = "${home.homeDirectory}/.local/share";
+          XDG_STATE_HOME = "${home.homeDirectory}/.local/state";
+        };
 
-    imports = [
-      ../modules/bash.nix
-      ../modules/git.nix
-      ../modules/zsh.nix
+        imports = [
+          ../modules/bash.nix
+          ../modules/git.nix
+          ../modules/zsh.nix
+        ];
+
+        home.packages = [
+          (pkgs.neovim {
+            withLspSupport = false;
+          })
+        ];
+      }
     ];
-
-    home.packages = [
-      (pkgs.neovim {
-        withLspSupport = false;
-      })
-    ];
-    programs = {
-      direnv = {
-        enable = true;
-        nix-direnv = { enable = true; };
-      };
-    };
   };
 }
